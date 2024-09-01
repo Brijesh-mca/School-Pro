2eaccbd856ff0e88a3c716c75635a0772d8aa85b
Brijesh-mca <ibrijesh.mca@gmail.com>
2024-09-01 00:50:33 -0500

    Adding student DashBoard

---
+++ Student/student.js
@@ -0,0 +1,52 @@
+
+let formDataList = [];
+
+document.getElementById("getdata").addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
+    let formData = {
+        Student: document.getElementById('Student').value,
+        Class: document.getElementById('Class').value,
+        Mobile: document.getElementById('Mobile').value,
+    };
+
+
+    displayArea();
+
+    document.getElementById("getdata").reset();
+
+    // adding data to array
+    formDataList.push(formData);
+
+})
+
+function displayArea() {
+    let forData = document.getElementById('forData');
+    
+
+
+    //crear the area
+
+    
+    forData.innerHTML = "";
+
+
+    formDataList.forEach((data, index) => {
+        let div = document.createElement('div');
+
+     
+
+        console.log(` Name - ${data.Student} ,Class - ${data.Class} , Mobile No. ${data.Mobile}`);
+                            
+                            
+        
+        div.textContent = ` Name - ${data.Student}  ,
+    Class - ${data.Class}  ,
+       Mobile No. - ${data.Mobile}` ;
+                           
+        forData.appendChild(div);
+  
+
+    })
+}
